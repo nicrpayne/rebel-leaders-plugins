79c8862fe28a9e7ca20f4be3ddb9da89a9fc85a0
nicrpayne <55965136+nicrpayne@users.noreply.github.com>
2026-03-13 11:59:56 -0400

    Scale deck cartridge another 20% bigger (158% x 156%)

--- client/src/components/CabinetDeck.tsx
+++ client/src/components/CabinetDeck.tsx
@@ -704,8 +704,8 @@ startTicker(msg, 14000, () => {
               left: '65%',
               top: '71%',
               transform: 'translate(-50%, -50%)',
-              width: '132%',
-              height: '130%',
+              width: '158%',
+              height: '156%',
             }}
           >
             <img
